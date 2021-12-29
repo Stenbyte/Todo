5a1011a642c544a57332dc403643fe351f05d9dd
Stenbyte <sten.scheifel@gmail.com>
2021-12-29 10:04:00 +0200

    first step

---
+++ src/components/Todos.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Todos: React.FC = () => {
+  return (
+    <div>
+      <header>
+        Header
+        <div>
+          <img src="" alt="logo" />
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Todos;
