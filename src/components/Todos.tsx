288de70565249cd65096d659b1eb6b6ac1665d83
Stenbyte <sten.scheifel@gmail.com>
2021-12-29 10:31:18 +0200

    header first step

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,14 +1,36 @@
 import React from "react";
-
+import styles from "../index.module.css";
 const Todos: React.FC = () => {
   return (
     <div>
       <header>
-        Header
-        <div>
-          <img src="" alt="logo" />
+        <div className={styles.top}>
+          <img src="/schedule.png" alt="logo" />
         </div>
+        <h1>Todo App</h1>
       </header>
+      <section>
+        <form>
+          <label htmlFor="note">Note:</label>
+          <input type="text" name="note" id="note" placeholder="Add noote..." />
+          <label htmlFor="date">Deadline</label>
+          <input type="datetime-local" name="date" id="date" />
+          <select>
+            <option value="">Please choose a status</option>
+            <option value="Todo">Todo</option>
+            <option value="Doing">Doing</option>
+            <option value="Done">Done</option>
+          </select>
+          <label htmlFor="picture">Add image (optional)</label>
+          <input
+            type="file"
+            name="picture"
+            id="picture"
+            accept="image/png, image/jpeg, image/jpg"
+          />
+          <button type="submit">Add</button>
+        </form>
+      </section>
     </div>
   );
 };
